bb0c5c903791347c4445cb9e7bc24a05f601985c
Jair León <leonrhj@gmail.com>
2023-05-14 22:25:59 -0600

    - Microservice installed. - Microservice first lines added to auth app - Dependencies updated

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -4,13 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+  const configSvc = app.get(ConfigService);
+  app.connectMicroservice({ transport: Transport.TCP });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
-  const configSvc = app.get(ConfigService);
+  await app.startAllMicroservices();
   await app.listen(configSvc.get('PORT'));
 }
 bootstrap();
