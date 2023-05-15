0f1fccb4b8dfbbe3e270e3d6ecc93f7f2421a615
Jair León <leonrhj@gmail.com>
2023-05-14 21:19:01 -0600

    - Jwt guard and strategy implemented. - Validate if user exists function developed.

---
+++ apps/auth/src/strategies/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersService } from '../users/users.service';
+import { Request } from 'express';
+import { TokenPayload } from '../interfaces/token-payload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    configSvc: ConfigService,
+    private readonly usersSvc: UsersService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => request?.cookies?.Authentication,
+      ]),
+      secretOrKey: configSvc.get('SECRET_RISKY'),
+    });
+  }
+
+  async validate({ userId }: TokenPayload) {
+    return this.usersSvc.getUser({ _id: userId });
+  }
+}
--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,13 +1,19 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import * as Bcrypt from 'bcryptjs';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersRepository } from './users.repository';
+import { GetUserDto } from './dto/get-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepo: UsersRepository) {}
 
   async create(createUserDto: CreateUserDto) {
+    await this.validateUserExists(createUserDto);
     return this.usersRepo.create({
       ...createUserDto,
       password: await Bcrypt.hash(createUserDto.password, 10),
@@ -21,5 +27,19 @@ export class UsersService {
       throw new UnauthorizedException('Email or Passwords not valid');
     }
     return user;
+  }
+
+  async getUser(getUserDto: GetUserDto) {
+    return this.usersRepo.findOne(getUserDto);
+  }
+
+  private async validateUserExists(createUserDto: CreateUserDto) {
+    try {
+      await this.usersRepo.findOne({ email: createUserDto.email });
+    } catch (error) {
+      return;
+    }
+
+    throw new UnprocessableEntityException('Email already exists');
   }
 }
